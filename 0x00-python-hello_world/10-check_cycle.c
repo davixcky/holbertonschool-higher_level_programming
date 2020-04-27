aac0d72c261d6c00edd821975e0d2421a0d565e9
David Orozco <dvdizcky@gmail.com>
2020-04-27 14:39:07 -0500

    Stepper logic changed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,18 +10,16 @@
  **/
 int check_cycle(listint_t *list)
 {
-	listint_t *s_low, *s_fast;
+	listint_t *s_slow, *s_fast;
 
-	s_low = s_fast = list;
-	s_fast = s_fast->next;
-
-	while(s_low != NULL && s_fast->next != NULL)
+	s_slow = s_fast = list;
+	while (s_slow != NULL && s_fast != NULL && s_fast->next != NULL)
 	{
-		if (s_low == s_fast)
-			return (1);
-
-		s_low = s_low->next;
+		s_slow = s_slow->next;
 		s_fast = s_fast->next->next;
+
+		if (s_slow == s_fast)
+			return (1);
 	}
 
 	return (0);

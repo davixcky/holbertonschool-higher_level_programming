e9f0f466aec58934d6e45485763d21ebd8b20d42
David Orozco <dvdizcky@gmail.com>
2020-04-30 12:08:29 -0500

    Task 1: Printer deleted

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,15 +2,11 @@
 from calculator_1 import add, sub, mul, div
 
 
-def printer(a, b, op, res):
-    print("{0} {2} {1} = {3}".format(a, b, op, res))
-
-
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    printer(a, b, '+', add(a, b))
-    printer(a, b, '-', sub(a, b))
-    printer(a, b, '*', mul(a, b))
-    printer(a, b, '/', div(a, b))
+    print('{} {} {} = {}'.format(a, '+', b, add(a, b)))
+    print('{} {} {} = {}'.format(a, '-', b, sub(a, b)))
+    print('{} {} {} = {}'.format(a, '*', b, mul(a, b)))
+    print('{} {} {} = {}'.format(a, '/', b, div(a, b)))

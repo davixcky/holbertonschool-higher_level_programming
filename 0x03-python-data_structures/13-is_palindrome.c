f4897e0b418fb69f0c7ba32b749723b6445f45db
David Orozco <dvdizcky@gmail.com>
2020-05-05 18:54:31 -0500

    Task 13: Malloc deleted, array in stack

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,18 +4,16 @@
 int is_palindrome(listint_t **head)
 {
 	int size;
-	int *options;
+	int options[50];
 	int i;
 
 	size = 0;
 	if (head == NULL || *head == NULL)
 		return (1);
 
-	options = malloc(sizeof(int));
 	while(*head != NULL)
 	{
 		size++;
-		options = realloc(options, sizeof(int) * size);
 		options[size - 1] = (*head)->n;
 		head = &(*head)->next;
 	}

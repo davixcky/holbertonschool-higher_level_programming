dbff692d30c8a7ba284b5728ce583d66a9574698
David Orozco <dvdizcky@gmail.com>
2020-05-05 18:49:37 -0500

    Task 13: Iterative way

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,26 +1,30 @@
 #include "lists.h"
 #include <stdlib.h>
 
-int isPalindromeUtil(listint_t **left, listint_t *right)
-{
-	int isp, isp1;
-
-	if (right == NULL)
-		return (1);
-
-	isp = isPalindromeUtil(left, right->next);
-	if (isp == 0)
-		return 0;
-
-	isp1 = (right->n == (*left)->n);
-
-	*left = (*left)->next;
-
-	return isp1;
-}
-
-// A wrapper over isPalindromeUtil()
 int is_palindrome(listint_t **head)
 {
-	return (isPalindromeUtil(head, *head));
+	int size;
+	int *options;
+	int i;
+
+	size = 0;
+	if (head == NULL || *head == NULL)
+		return (1);
+
+	options = malloc(sizeof(int));
+	while(*head != NULL)
+	{
+		size++;
+		options = realloc(options, sizeof(int) * size);
+		options[size - 1] = (*head)->n;
+		head = &(*head)->next;
+	}
+
+	for (i = 0; i < size / 2; i++)
+	{
+		if (options[i] != options[size - i - 1])
+			return (0);
+	}
+
+	return (1);
 }

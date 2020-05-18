7179608bf456646a284c1857efc97614c7d7428d
David Orozco <dvdizcky@gmail.com>
2020-05-18 12:36:38 -0500

    Task 7: TypeError added to except

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,7 +7,7 @@ def safe_print_integer_err(value):
 
     try:
         print('{:d}'.format(value))
-    except ValueError as err:
+    except (ValueError, TypeError) as err:
         res = False
         print('Exception: {}'.format(err), file=stderr)
 

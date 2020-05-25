0e333d8b2a17831c0452454391f558a96887a8b5
David Orozco <dvdizcky@gmail.com>
2020-05-25 12:39:25 -0500

    Task 3 added

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+'''Class rectangle'''
+
+
+class Rectangle:
+    '''Form'''
+
+    def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+
+        if width < 0:
+            raise ValueError('width must be >= 0')
+
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+
+        if height < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        w, h = self.__width, self.__height
+
+        if w == 0 or h == 0:
+            return 0
+
+        return (w * 2) + (h * 2)
+
+    def __str__(self):
+        w, h = self.__width, self.__height
+        return '{}{}'.format(('#' * w + '\n') * (h - 1), '#' * w)

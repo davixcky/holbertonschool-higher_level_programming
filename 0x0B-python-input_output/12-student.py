5941e802931fed5b387674ae4e7a98f9ad2f8293
David Orozco <dvdizcky@gmail.com>
2020-06-03 16:59:55 -0500

    Task 12: PEP8 style applied

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -13,7 +13,7 @@ class Student:
 
     def to_json(self, attrs=None):
         '''JSON representation of an instance'''
-        if attrs == None:
+        if attrs is None:
             return self.__dict__
 
         attrs_dict = {}

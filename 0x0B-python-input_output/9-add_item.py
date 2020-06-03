c3dfa21da2ee8826b31a920069f362b93021ae1c
David Orozco <dvdizcky@gmail.com>
2020-06-03 17:04:49 -0500

    Task 9: Error handling implmented

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,5 +4,9 @@ import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-data = load_from_json_file('add_item.json')
+try:
+    data = load_from_json_file('add_item.json')
+except:
+    data = []
+
 save_to_json_file(data + sys.argv[1:], 'add_item.json')

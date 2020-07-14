cebe013b238d0c69caff04307c5fa7f1218dc7e4
David Orozco <dvdizcky@gmail.com>
2020-07-14 15:13:31 -0500

    Task 2: New line added

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,3 +4,4 @@ DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
+

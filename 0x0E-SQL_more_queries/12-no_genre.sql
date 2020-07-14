a007287404aa6c61ef3a827c5e1f1f45769f190c
David Orozco <dvdizcky@gmail.com>
2020-07-14 14:12:43 -0500

    Task 12: Order applied

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,6 @@ SELECT m.title "title", sg.genre_id "genre_id"
 FROM tv_shows m
 LEFT JOIN tv_show_genres sg
 	ON sg.show_id = m.id
-	WHERE sg.show_id IS NULL;
+	WHERE sg.show_id IS NULL
+ORDER BY m.title;
 

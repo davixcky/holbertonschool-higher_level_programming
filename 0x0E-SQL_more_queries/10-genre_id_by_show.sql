062d5ecdd3ae151b4908b033e9838422347af786
David Orozco <dvdizcky@gmail.com>
2020-07-14 13:55:38 -0500

    Task 10: Sql commands uppercase

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,3 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-select m.title "title", g.id "genre_id" from tv_shows m, tv_genres g, tv_show_genres sg where sg.genre_id = g.id and sg.show_id = m.id order by m.title, g.id;
+SELECT m.title "title", g.id "genre_id" FROM tv_shows m, tv_genres g, tv_show_genres sg WHERE sg.genre_id = g.id AND sg.show_id = m.id ORDER BY m.title, g.id;
+

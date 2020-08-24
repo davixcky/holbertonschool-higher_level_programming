3f7873f88fea6b41d3776c650461231dc9665646
Orozco David <dvdizcky@gmail.com>
2020-08-24 15:28:29 -0500

    Local url instead of public

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    url = 'http://e3372298d263.19.hbtn-cod.io:5000/search_user'
+    url = 'http://0.0.0.0:5000/search_user'
     letter = argv[1] if len(argv) > 1 else ""
 
     s = requests.Session()

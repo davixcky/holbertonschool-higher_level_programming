367ce2da9a92eda87035e943b690b1e60eab2dc7
Orozco David <dvdizcky@gmail.com>
2020-08-24 16:08:27 -0500

    Task 10 added

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Fetches https://intranet.hbtn.io/status"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+
+if __name__ == '__main__':
+    user, repo = argv[1:]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(user, repo)
+
+    s = requests.Session()
+
+    response = s.get(url)
+    data = response.json()
+    for i in range(10):
+        obj = data[i]
+        sha, username = obj['sha'], obj['commit']['author']['name']
+        print('{}: {}'.format(sha, username))

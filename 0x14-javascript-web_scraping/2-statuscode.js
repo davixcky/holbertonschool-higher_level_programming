6636ec343f5862fe77cfc78143c8b1159845595d
Orozco David <dvdizcky@gmail.com>
2020-09-22 17:31:58 -0500

    Task 2: Output fixed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 
 request(process.argv[2], { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
-  console.log(res.statusCode);
+  console.log('code: ' + res.statusCode);
 });
